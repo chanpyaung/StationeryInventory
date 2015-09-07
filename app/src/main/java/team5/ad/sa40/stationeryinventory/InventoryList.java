8429a4bab01e03e74451edecc09958675482b057
a0070011 <a0070011@u.nus.edu>
2015-09-07 09:30:24 +0800

    edited inventory list filters

--- app/src/main/java/team5/ad/sa40/stationeryinventory/InventoryList.java
+++ app/src/main/java/team5/ad/sa40/stationeryinventory/InventoryList.java
@@ -27,8 +27,9 @@ import team5.ad.sa40.stationeryinventory.Model.Retrieval;
 public class InventoryList extends android.support.v4.app.Fragment {
 
 
-    private String empID;
-    private String[] filters = {"View All","Pending","Retrieved"};
+    private String[] categories = {"All Categories","Clip","Envelope","Eraser","Exercise","File","Pen","Puncher",
+            "Pad","Paper","Ruler","Scissors","Tape","Sharpener","Shorthand","Stapler","Tacks","Tparency","Tray"};
+    private String[] filters = {"View All","Low Stock","Available"};
     private List<Item> allInv;
     private RecyclerView mRecyclerView;
     private InvListAdapter adapter;
@@ -68,10 +69,86 @@ public class InventoryList extends android.support.v4.app.Fragment {
                         showAllInventory();
                         break;
                     case (1):
-                        //showPendingInventory();
+                        showLowInventory();
                         break;
                     case (2):
-                        //showRetrievedInventory();
+                        showAvailableInventory();
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                showAllInventory();
+            }
+        });
+
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String>(this.getActivity(),android.R.layout.simple_spinner_item,categories);
+        FiltersAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerInvCat.setAdapter(FiltersAdapter);
+
+        spinnerInvCat.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(InventoryList.this.getActivity(), "Selected: " + position, Toast.LENGTH_SHORT).show();
+                Log.i("spinner's:", categories[position]);
+                switch (position) {
+                    case (0):
+                        showAllInventory();
+                        break;
+                    case (1):
+                        showCategoryItems(1);
+                        break;
+                    case (2):
+                        showCategoryItems(2);
+                        break;
+                    case (3):
+                        showCategoryItems(3);
+                        break;
+                    case (4):
+                        showCategoryItems(4);
+                        break;
+                    case (5):
+                        showCategoryItems(5);
+                        break;
+                    case (6):
+                        showCategoryItems(6);
+                        break;
+                    case (7):
+                        showCategoryItems(7);
+                        break;
+                    case (8):
+                        showCategoryItems(8);
+                        break;
+                    case (9):
+                        showCategoryItems(9);
+                        break;
+                    case (10):
+                        showCategoryItems(10);
+                        break;
+                    case (11):
+                        showCategoryItems(11);
+                        break;
+                    case (12):
+                        showCategoryItems(12);
+                        break;
+                    case (13):
+                        showCategoryItems(13);
+                        break;
+                    case (14):
+                        showCategoryItems(14);
+                        break;
+                    case (15):
+                        showCategoryItems(15);
+                        break;
+                    case (16):
+                        showCategoryItems(16);
+                        break;
+                    case (17):
+                        showCategoryItems(17);
+                        break;
+                    case (18):
+                        showCategoryItems(18);
                         break;
                 }
             }
@@ -110,4 +187,39 @@ public class InventoryList extends android.support.v4.app.Fragment {
         });
     }
 
+    public void showLowInventory() {
+        List<Item> lowStockItems = new ArrayList<Item>();
+        for(int i=0; i<allInv.size(); i++) {
+            Item item = allInv.get(i);
+            if(item.getStock() < item.getRoLvl()) {
+                lowStockItems.add(item);
+            }
+        }
+        adapter.mItems = lowStockItems;
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    public void showAvailableInventory() {
+        List<Item> availableItems = new ArrayList<Item>();
+        for(int i=0; i<allInv.size(); i++) {
+            Item item = allInv.get(i);
+            if(!(item.getStock() < item.getRoLvl())) {
+                availableItems.add(item);
+            }
+        }
+        adapter.mItems = availableItems;
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    public void showCategoryItems(int catID) {
+        List<Item> categoryItems = new ArrayList<Item>();
+        for(int i=0; i<allInv.size(); i++) {
+            Item item = allInv.get(i);
+            if(item.getItemCatID() == catID) {
+                categoryItems.add(item);
+            }
+        }
+        adapter.mItems = categoryItems;
+        mRecyclerView.setAdapter(adapter);
+    }
 }
